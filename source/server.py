70916cd1ebf2012e1544ee33a83f9e4c0ee310c5
BachinDanila <nikto985@gmail.com>
2018-07-17 12:42:48 +1000

    Add Sqlite Database

--- source/server.py
+++ source/server.py
@@ -3,10 +3,18 @@ from http.server import BaseHTTPRequestHandler,HTTPServer
 import os
 import base64
 import pickle
+import sqlite3
+import json
+import datetime
+import time
+import threading
 
-addr = '10.7.130.13'
+
+addr = '192.168.43.150'
 port = 80
 global answer
+global file
+file = 'data.db'
 
 def get_parent_dir(directory):
     import os
@@ -15,7 +23,7 @@ def get_parent_dir(directory):
 parent = get_parent_dir(os.getcwd())
 os.chdir("{0}/data".format(parent))
 
-open('data.txt', 'wb').close()                                               #Удаление данных из data
+#open('data.txt', 'wb').close()                                               #Удаление данных из data
 
 #Create custom HTTPRequestHandler class
 class HTTPRequestHandler(BaseHTTPRequestHandler):
@@ -29,12 +37,12 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
             
         if 'get_data' in request:
             size = int(request.split(' ')[1])
-            self.send_data(size)
-            print(request)
+            self.send_esp_data(size)
+            #print(request)
             
         else:    
-            self.store_data(request)
-            print(request)
+            self.store_esp_data(request)
+            #print(request)
             
             #send code 200 response
             self.send_response(200)
@@ -47,22 +55,27 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
             self.wfile.write(answer.encode())
         return
     
-    def store_data(self,data):
-        file = open('data.txt','a')
-        file.write(data + "\n")
-        file.close()  
+    def store_esp_data(self,data):
+        conn = sqlite3.connect(file)
+        cursor = conn.cursor()
+        time1 = time.time()
+        ax,ay,az,gx,gy,gz = data.split(' ')
+        data1 = [(time1,ax,ay,az,gx,gy,gz)]
+        cursor.executemany("INSERT INTO esp(time,ax,ay,az,gx,gy,gz) VALUES (?,?,?,?,?,?,?)", data1)
+        conn.commit()
+        conn.close()
         
-    def send_data(self,size):
-        data = ""
-        file = open('data.txt','rb')
-        
-        for i in range(size):
-            data += file.readline().decode()
-        self.send_response(200)
-        self.send_header('Content-type','text-html')
-        self.end_headers()
-        #send file content to client
-        self.wfile.write(data.encode())
+    def send_esp_data(self,size):
+        conn = sqlite3.connect(file)
+        cursor = conn.cursor()
+        sql = "SELECT * FROM esp WHERE id < ?"
+        cursor.execute(sql,[(size)])
+        l = cursor.fetchall()
+        arg = ('id','time','ax','ay','az','gx','gy','gz')
+        dic = [dict(zip(arg,i)) for i in l]
+        jsonarray = json.dumps(dic, ensure_ascii=False)
+        print(jsonarray)
+        conn.close()
         
 def run():
     print('http server is starting...')
